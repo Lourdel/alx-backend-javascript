dba9542b34d5ada9a3789a2e7cf571f29a615cb2
lourdel <klourdel@gmail.com>
2023-04-26 00:38:40 +0300

    map method

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,8 +1,7 @@
 export default function hasValuesFromArray(set, array) {
   for (const element of array) {
     if (!set.has(element)) {
-
-    return false;
+      return false;
     }
   }
   return true;
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   const result = [];
   for (const value of set) {
-    if (typeof value ==='string' && value.startsWith(startString)) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
       result.push(value.slice(startString.length));
     }
   }
