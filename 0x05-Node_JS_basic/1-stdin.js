93543b4392050b181349033428fe6341bda4abb3
lourdel <klourdel@gmail.com>
2023-06-14 23:10:57 +0300

    Process-stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,14 +2,16 @@
 //process.stdin
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('readable', () => {
-  const data = process.stdin.read();
 
-  if (data) {
-    process.stdout.write(`Your name is: ${data}`);
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', () => {
+  const buf = process.stdin.read();
+
+  if (buf !== null) {
+    process.stdout.write(`Your name is: ${buf}`);
   }
 });
-
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
