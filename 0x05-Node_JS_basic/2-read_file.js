371f74872451d3f2dde9cbb690b713ced48393f5
lourdel <klourdel@gmail.com>
2023-06-16 18:40:26 +0300

    read_file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,24 +1,25 @@
 const fs = require('fs');
 
-const countStudents = (path) => {
+const countStudents = (fileName) => {
   let csStudents = [];
   const fields = [];
   try {
-    const data = fs.readFileSync(path)
-    const dataObj = data.toString().trim().split('\n');
+    const data = fs.readFileSync(fileName);
+    const strData = data.toString().trim();
+    const dataObj = strData.split('\n');
     const listCreate = (val) => val.trim().split(',');
-    const new_obj = dataObj.map(listCreate);
-    console.log(`Number of students: ${new_obj.length - 1}`);
-    for (let i = 0; i < new_obj.length; i += 1) {
-      fields.push(new_obj[i][3]);
+    const modObj = dataObj.map(listCreate);
+    console.log(`Number of students: ${modObj.length - 1}`);
+    for (let i = 0; i < modObj.length; i += 1) {
+      fields.push(modObj[i][3]);
     }
     fields.shift();
     const uniqueFields = [...new Set(fields)];
     for (const x in uniqueFields) {
       if (Object.prototype.hasOwnProperty.call(uniqueFields, x)) {
-        for (let i = 0; i < new_obj.length; i += 1) {
-          if (new_obj[i].includes(uniqueFields[x])) {
-            csStudents.push(new_obj[i][0]);
+        for (let i = 0; i < modObj.length; i += 1) {
+          if (modObj[i].includes(uniqueFields[x])) {
+            csStudents.push(modObj[i][0]);
           }
         }
         console.log(`Number of students in ${uniqueFields[x]}: ${csStudents.length}. List: ${csStudents.join(', ')}`);
