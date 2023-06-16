125fc1cf7edee6cc417461520717c7deb9dfc484
lourdel <klourdel@gmail.com>
2023-06-16 18:17:57 +0300

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,11 @@
-#!/usr/bin/env node
-//process.stdin
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', () => {
-  const buf = process.stdin.read();
-
-  if (buf !== null) {
-    process.stdout.write(`Your name is: ${buf}`);
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 process.stdin.on('end', () => {
